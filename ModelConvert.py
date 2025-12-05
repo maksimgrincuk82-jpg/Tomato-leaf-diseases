77940794e220525577f49c0ef45285804ecb66af
MaksAkvar <maksimgrincuk82@gmail.com>
2025-12-05 16:51:26 +0200

    Update ModelConvert.py

--- ModelConvert.py
+++ ModelConvert.py
@@ -1,15 +1,13 @@
 import torch
 import timm
 
-# створюємо ту ж саму архітектуру
 model = timm.create_model("vit_base_patch16_224", pretrained=False, num_classes=11)
 model.load_state_dict(torch.load("vit_tomato_model.pth", map_location="cpu"))
 model.eval()
 
-# тестовий вхід
 example = torch.randn(1, 3, 224, 224)
 
-# трасування (перетворення у TorchScript)
 traced_model = torch.jit.trace(model, example)
 traced_model.save("vit_tomato_model_lite.pt")
-print("✅ Збережено у форматі TorchScript!")
+print("Збережено у форматі TorchScript!")
+
